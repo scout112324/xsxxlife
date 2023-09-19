d4a53b764e845f2c1cefef54513eced787950bde
陈卫卫 <chenweiwei@mti-sh.cn>
2023-09-19 11:18:40 +0800

    feat: 我的基本页面搭建

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,15 +1,22 @@
 <template>
 	<view class="user-page">
+		<u-navbar title="我的" :bgColor="bgColor" leftIcon="" :titleStyle="titleStyle" placeholder>
+			<view class="u-nav-slot" slot="left"></view>
+		</u-navbar>
 		<view class="user-info">
-			<view class="title">
-				登录~~~~
+			<view class="user">
+				<image class="avatar"
+					src="https://tse4-mm.cn.bing.net/th/id/OIP-C.vNfhdk-LRUEgI7aMQdSwYgAAAA?pid=ImgDet&rs=1" mode="">
+				</image>
+				<view class="nickname">
+					ketty perry
+				</view>
 			</view>
-			<button type="default" class="login" @click="handleLogin">授权登录</button>
 		</view>
 		<view class="user-list">
 			<view class="user-item" v-for="item in userData" :key="item.id">
 				<view class="icon">
-					<uni-icons :type="item.icon" size="22"></uni-icons>
+					<image class="icon-image" :src="item.icon" mode=""></image>
 				</view>
 				<view class="content">
 					<view class="name">
@@ -19,21 +26,7 @@
 				</view>
 			</view>
 		</view>
-		<!-- 普通弹框 -->
-		<uni-popup ref="popup" background-color="#fff" class="login-container">
-			<view class="popup-content">
-				<view class="title">
-					<uni-icons type="closeempty" size="22" @click="handleclose"></uni-icons>
-				</view>
-				<view class="content">
-					<image class="logo"
-						src="https://tse3-mm.cn.bing.net/th/id/OIP-C.W38cHNbpeslJrW3v0DyP4QHaEK?pid=ImgDet&rs=1"
-						mode=""></image>
-					<button type="default" class="login" @click="handleLogin">授权登录</button>
-					<label class="radio"><radio :checked="checked" @click="handleChecked" />我已阅读并同意</label>
-				</view>
-			</view>
-		</uni-popup>
+
 	</view>
 </template>
 
@@ -41,92 +34,125 @@
 	export default {
 		data() {
 			return {
+				bgColor: "#FBE94E",
+				titleStyle: {
+					fontWeight: 500,
+					color: "#131313"
+				},
+				systemBarHeight: 0,
 				userData: [{
 						id: 1,
 						name: "收藏列表",
-						icon: "star"
+						icon: "../../static/user/shoucang.png"
 					},
 					{
 						id: 2,
 						name: "点赞列表",
-						icon: "hand-up"
+						icon: "../../static/user/dianzan.png"
 					},
 					{
 						id: 3,
 						name: "我的活动",
-						icon: "flag"
+						icon: "../../static/user/huodong.png"
 					},
 					{
 						id: 4,
-						name: "交易记录",
-						icon: "star"
+						name: "我的发布",
+						icon: "../../static/user/fabu.png"
 					},
 					{
 						id: 5,
-						name: "我的发布",
-						icon: "star"
+						name: "交易记录",
+						icon: "../../static/user/jiaoyi.png"
 					},
+					
 					{
 						id: 6,
-						name: "聊天列表",
-						icon: "chat"
+						name: "联系客服",
+						icon: "../../static/user/kefu.png"
 					},
 					{
 						id: 7,
-						name: "联系客服",
-						icon: "chat"
-					},
-					{
-						id: 8,
 						name: "意见反馈",
-						icon: "chat"
+						icon: "../../static/user/yijian.png"
 					}
 				],
 				checked: false
 			}
 		},
+		created() {
+
+		},
 		methods: {
-			// 登录
-			handleLogin() {
-				this.$refs.popup.open('bottom')
-			},
-			handleChecked() {
-				console.log(!this.checked)
-				this.checked = !this.checked
-			},
-			handleclose() {
-				this.$refs.popup.close();
-			}
+
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
 	.user-page {
-		.user-info {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
+		background-color: #F3F6F5;
+		height: 100vh;
 
-			.login {
-				height: 80rpx;
-				line-height: 80rpx;
-				width: 60%;
-				color: #ffffff;
-				background-color: #1841f4;
-				border-radius: 40rpx;
+		.user-info {
+			height: 92rpx;
+			background: linear-gradient(90deg, #FBE94E 0%, #F9DC4A 100%);
+			border-bottom-left-radius: 30rpx;
+			border-bottom-right-radius: 30rpx;
+			box-sizing: border-box;
+
+			.user {
+				margin: 0 15rpx;
+				height: 184rpx;
+				background: #FFFFFF;
+				box-shadow: 0rpx 2rpx 24rpx 0rpx rgba(0, 0, 0, 0.04);
+				border-radius: 20rpx;
+				display: flex;
+				align-items: center;
+
+				.avatar {
+					width: 96rpx;
+					height: 96rpx;
+					border-radius: 48rpx;
+					border: 1rpx solid #FFD100;
+					margin: 0 30rpx;
+				}
+				
+				.nickname {
+					font-size: 35rpx;
+					font-family: PingFangSC-Medium, PingFang SC;
+					font-weight: 500;
+					color: #232624;
+					line-height: 49rpx;
+				}
 			}
 		}
 
 		.user-list {
+			margin: 112rpx 15rpx 15rpx 15rpx;
+			background: #FFFFFF;
+			box-shadow: 0rpx 2rpx 24rpx 0rpx rgba(0, 0, 0, 0.04);
+			border-radius: 20rpx;
+
 			.user-item {
 				display: flex;
 				justify-content: space-between;
 				align-items: center;
+				padding: 34rpx 0rpx;
+				margin: 0 30rpx;
+
+				&:not(:last-child) {
+					border-bottom: 1px solid #EDEDED;
+				}
+
 
 				.icon {
-					width: 100rpx;
 					text-align: center;
+
+					.icon-image {
+						width: 35rpx;
+						height: 34rpx;
+					}
 				}
 
 				.content {
@@ -134,8 +160,19 @@
 					display: flex;
 					justify-content: space-between;
 					align-items: center;
-					border-bottom: 1px solid #ccc;
-					padding: 20rpx 0;
+
+					.name {
+						margin-left: 20rpx;
+						font-size: 30rpx;
+						font-family: PingFangSC-Regular, PingFang SC;
+						font-weight: 400;
+						color: #4D504F;
+						line-height: 42rpx;
+					}
+
+					::v-deep .uni-icons {
+						color: #AAAAAA !important;
+					}
 				}
 			}
 		}
@@ -143,6 +180,7 @@
 		.login-container {
 			.popup-content {
 				padding: 20rpx;
+
 				.title {
 					text-align: right;
 				}
@@ -151,6 +189,7 @@
 					display: flex;
 					flex-direction: column;
 					align-items: center;
+
 					.logo {
 						width: 160rpx;
 						height: 160rpx;
