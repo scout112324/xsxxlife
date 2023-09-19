fa267af0462c8c2b790d41a8486d2d3029527db1
陈卫卫 <chenweiwei@mti-sh.cn>
2023-09-19 13:34:29 +0800

    feat: 附近社群页面搭建

--- pages/index/service/community.vue
+++ pages/index/service/community.vue
@@ -1,7 +1,9 @@
 <template>
 	<view class="community-page">
-		<u-navbar title="附近社群" :bgColor="bgColor" leftIcon="" :titleStyle="titleStyle" placeholder>
-			<view class="u-nav-slot" slot="left"></view>
+		<u-navbar title="附近社群" :bgColor="bgColor" :titleStyle="titleStyle" placeholder>
+			<view class="u-nav-slot" slot="left">
+				<u-icon name="arrow-left" size="18" @click="handleBack"></u-icon>
+			</view>
 		</u-navbar>
 		<view class="community-container">
 			<view class="community-list">
@@ -36,29 +38,35 @@
 			}
 		},
 		methods: {
-			
+			handleBack() {
+				uni.switchTab({
+					url: "/pages/index/index"
+				})
+			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-.community-page {
-	background-color: #F3F6F5;
-	.community-container {
-		height: 92rpx;
-		background: linear-gradient(90deg, #FBE94E 0%, #F9DC4A 100%);
-		border-bottom-left-radius: 30rpx;
-		border-bottom-right-radius: 30rpx;
-		box-sizing: border-box;
-		.community-list {
-			margin: 0rpx 15rpx;
-			height: 1150rpx;
-			overflow-y: auto;
-			background: #FFFFFF;
-			box-shadow: 0rpx 2rpx 24rpx 0rpx rgba(0, 0, 0, 0.04);
-			border-radius: 20rpx;
-			padding: 10rpx 20rpx;
+	.community-page {
+		background-color: #F3F6F5;
+
+		.community-container {
+			height: 92rpx;
+			background: linear-gradient(90deg, #FBE94E 0%, #F9DC4A 100%);
+			border-bottom-left-radius: 30rpx;
+			border-bottom-right-radius: 30rpx;
+			box-sizing: border-box;
+
+			.community-list {
+				margin: 0rpx 15rpx;
+				height: 1150rpx;
+				overflow-y: auto;
+				background: #FFFFFF;
+				box-shadow: 0rpx 2rpx 24rpx 0rpx rgba(0, 0, 0, 0.04);
+				border-radius: 20rpx;
+				padding: 10rpx 20rpx;
+			}
 		}
 	}
-}
 </style>
