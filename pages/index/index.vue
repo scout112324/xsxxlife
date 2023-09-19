e5062dbc2663eee8824f840c76da61deaf96c8cd
陈卫卫 <chenweiwei@mti-sh.cn>
2023-09-19 11:31:50 +0800

    feat: 首页样式修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -11,10 +11,12 @@
 				</view>
 			</view>
 		</u-navbar>
+		<view class="occupy"></view>
 		<view class="content-page">
 			<view class="notice">
-				<image class="jiebao" src="../../static/home/jiebao.png" mode=""></image>
-				<text class="content">发布闲置后支持线上线下交易啦～</text>
+				<!-- <image class="jiebao" src="../../static/home/jiebao.png" mode=""></image> -->
+				<!-- <text class="content">发布闲置后支持线上线下交易啦～</text> -->
+				<u-notice-bar :text="text"></u-notice-bar>
 			</view>
 			<view class="service">
 				<view class="item" v-for="item in serviceData" :key="item.id" @click="handleClickService(item.name)">
@@ -68,6 +70,7 @@
 					color: "#131313"
 				},
 				cityname: "上海市",
+				text: "发布闲置后支持线上线下交易啦～",
 				serviceData: [{
 						id: 1,
 						name: "综合服务",
@@ -202,10 +205,16 @@
 				}
 			}
 		}
-
+		.occupy {
+			height: 50rpx;
+			background: linear-gradient(90deg, #FBE94E 0%, #F9DC4A 100%);
+		}
 		.content-page {
+			margin-top: -50rpx;
 			background-color: #F1F1F2;
 			padding: 24rpx 15rpx;
+			border-top-left-radius: 50rpx;
+			border-top-right-radius: 50rpx;
 
 			.notice {
 				display: flex;
@@ -214,20 +223,31 @@
 				background: #FFFFFF;
 				box-shadow: 0rpx 2rpx 24rpx 0rpx rgba(0, 0, 0, 0.04);
 				border-radius: 16rpx;
-
-				.jiebao {
-					width: 40rpx;
-					height: 32rpx;
-					margin: 0 10rpx 0 18rpx;
-				}
-
-				.content {
+				
+				::v-deep .u-notice-bar {
+					background: #FFFFFF !important;
+					box-shadow: 0rpx 2rpx 24rpx 0rpx rgba(0,0,0,0.04);
+					border-radius: 16rpx;
+					
 					font-size: 26rpx;
 					font-family: PingFangSC-Regular, PingFang SC;
 					font-weight: 400;
 					color: #855C15;
-					line-height: 26rpx;
 				}
+
+				// .jiebao {
+				// 	width: 40rpx;
+				// 	height: 32rpx;
+				// 	margin: 0 10rpx 0 18rpx;
+				// }
+
+				// .content {
+				// 	font-size: 26rpx;
+				// 	font-family: PingFangSC-Regular, PingFang SC;
+				// 	font-weight: 400;
+				// 	color: #855C15;
+				// 	line-height: 26rpx;
+				// }
 			}
 
 			.service {
