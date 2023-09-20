35d999cb21f6afd20740195fad34fabde4943135
陈卫卫 <chenweiwei@mti-sh.cn>
2023-09-20 10:15:53 +0800

    feat: 闲置详情页面搭建

---
+++ components/comment.vue
@@ -0,0 +1,160 @@
+<template>
+	<view class="parent">
+		<view class="comment-num">
+			共4条评论
+		</view>
+		<view class="comment-content">
+			<input class="uni-input" placeholder="感兴趣的话可以留下你的评论呦～" :value="commentValue" @input="clearInput" />
+		</view>
+		<view class="user-info">
+			<image class="avatar"
+				src="https://bpic.588ku.com/element_origin_min_pic/20/11/04/6b53830d8c8582ddc0f6fc2eabf67cac.jpg"
+				mode=""></image>
+			<view class="name">
+				大美
+				<text class="address">江苏</text>
+			</view>
+		</view>
+		<view class="content">
+			你好呀，请问租出去了吗？租出去了吗？租出去了吗？租出去了吗？
+		</view>
+		<view class="btn">
+			<view class="time">
+				40分钟前
+			</view>
+			<view class="record">
+				回复
+			</view>
+		</view>
+		<view class="children">
+			<view class="user-info">
+				<image class="avatar"
+					src="https://bpic.588ku.com/element_origin_min_pic/20/11/04/6b53830d8c8582ddc0f6fc2eabf67cac.jpg"
+					mode=""></image>
+				<view class="name">
+					大美
+				</view>
+			</view>
+			<view class="content">
+				你好呀，请问租出去了吗？租出去了吗？租出去了吗？租出去了吗？
+			</view>
+			<view class="btn">
+				<view class="time">
+					40分钟前
+				</view>
+				<view class="record">
+					回复
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				commentValue: ""
+			}
+		},
+		methods: {
+			clearInput() {}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.parent {
+		padding: 40rpx 0;
+
+		.comment-num {
+			font-size: 30rpx;
+			font-family: PingFangSC-Medium, PingFang SC;
+			font-weight: 500;
+			color: #232624;
+			line-height: 42rpx;
+			margin-bottom: 40rpx;
+		}
+
+		.comment-content {
+			margin-bottom: 48rpx;
+
+			.uni-input {
+				box-sizing: border-box;
+				width: 690rpx;
+				height: 58rpx;
+				background: #F6F4F4;
+				border-radius: 29rpx;
+				padding: 10rpx 22rpx;
+			}
+		}
+	}
+
+	.parent,
+	.children {
+		margin: 30rpx;
+
+		.user-info {
+			display: flex;
+			justify-content: flex-start;
+			align-items: center;
+
+			.avatar {
+				width: 58rpx;
+				height: 58rpx;
+				border-radius: 29rpx;
+				margin-right: 14rpx;
+			}
+
+			.name {
+				font-size: 26rpx;
+				font-family: PingFangSC-Medium, PingFang SC;
+				font-weight: 500;
+				color: #949494;
+				line-height: 37rpx;
+				
+
+				.address {
+					font-size: 18rpx;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #949494;
+					line-height: 37rpx;
+					background-color: #F6F6F6;
+					margin-left: 6rpx;
+				}
+			}
+		}
+
+		.content {
+			margin-left: 72rpx;
+			font-size: 26rpx;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #232624;
+			line-height: 37rpx;
+		}
+
+		.btn {
+			margin-left: 72rpx;
+			font-family: Source Han Sans SC VF;
+			font-size: 20rpx;
+			font-style: normal;
+			font-weight: 400;
+			line-height: normal;
+
+			display: flex;
+			justify-content: flex-start;
+			align-items: center;
+
+			.time {
+				color: rgba(147, 153, 163, 0.60);
+			}
+
+			.record {
+				color: #FFBD3C;
+				margin-left: 20rpx;
+			}
+		}
+	}
+</style>
--- pages/unused/detailUnused/detail.vue
+++ pages/unused/detailUnused/detail.vue
@@ -22,19 +22,63 @@
 				<text class="unit">¥</text>
 				<text class="num">279000</text>
 			</view>
+			<view class="content">
+				出闲置全新！感兴趣的朋友看过来或者在线直接联系，Apple/IPhone15一台，国行版内128G，手机无任何维修记录，没有任何划痕！
+			</view>
+			<view class="product-image">
+				<image class="image"
+					src="https://tse1-mm.cn.bing.net/th/id/OIP-C.PutJRYbN20MeTUKQCLFAZQHaHa?pid=ImgDet&rs=1" mode="">
+				</image>
+				<image class="image"
+					src="https://tse1-mm.cn.bing.net/th/id/OIP-C.PutJRYbN20MeTUKQCLFAZQHaHa?pid=ImgDet&rs=1" mode="">
+				</image>
+			</view>
+		</view>
+		<view class="comments">
+			<comment></comment>
+		</view>
+		<view class="footer">
+			<view class="btns">
+				<view class="item">
+					<image class="icon" src="../../../static/components/dianzan.png" mode=""></image>
+					<view class="num">
+						999
+					</view>
+				</view>
+				<view class="item">
+					<image class="icon" src="../../../static/components/shoucang_set.png" mode=""></image>
+					<view class="num">
+						999
+					</view>
+				</view>
+				<view class="item">
+					<image class="icon" src="../../../static/components/pinglun.png" mode=""></image>
+					<view class="num">
+						999
+					</view>
+				</view>
+			</view>
+			<view class="communicate">
+				<u-button icon="share-square" text="联系卖家" @click="handleCommuniteClick"></u-button>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
+	import comment from "@/components/comment.vue"
 	export default {
+		components: {
+			comment
+		},
 		data() {
 			return {
 
 			}
 		},
 		methods: {
-			handleShareClick() {}
+			handleShareClick() {},
+			handleCommuniteClick() {}
 		}
 	}
 </script>
@@ -45,7 +89,7 @@
 
 		.container {
 			background-color: #ffffff;
-			padding: 46rpx 30rpx;
+			padding: 46rpx 30rpx 20rpx 30rpx;
 
 			.header {
 				display: flex;
@@ -112,6 +156,78 @@
 
 				.num {
 					font-size: 46rpx;
+				}
+			}
+
+			.content {
+				font-size: 28rpx;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #232624;
+				line-height: 44rpx;
+				margin: 25rpx 0;
+			}
+			.product-image {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				.image {
+					width: 689rpx;
+					height: 659rpx;
+					border-radius: 10rpx;
+					margin-bottom: 20rpx;
+				}
+			}
+		}
+		.comments {
+			background-color: #ffffff;
+		}
+		.footer {
+			background-color: #ffffff;
+			padding: 0 34rpx;
+			margin-top: 24rpx;
+			height: 157rpx;
+			
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			border-top: 1px solid #D8D8D;
+			
+			.btns {
+				display: flex;
+				align-items: center;
+				.item {
+					display: flex;
+					align-items: center;
+					margin-right: 43rpx;
+					.icon {
+						width: 33rpx;
+						height: 35rpx;
+					}
+					.num {
+						margin-left: 15rpx;
+						font-size: 24rpx;
+						font-family: PingFangSC-Medium, PingFang SC;
+						font-weight: 500;
+						color: #232624;
+						line-height: 33rpx;
+					}
+				}
+			}
+			.communicate {
+				::v-deep .u-button {
+					width: 224rpx;
+					height: 74rpx;
+					background: #FFD100;
+					border-radius: 46rpx;
+					border: 0 solid rgba(255, 209, 0, 0.31);
+				}
+				
+				::v-deep .u-button__text {
+					font-size: 30rpx;
+					font-family: PingFangSC-Medium, PingFang SC;
+					font-weight: 500;
+					color: #232624;
 				}
 			}
 		}
