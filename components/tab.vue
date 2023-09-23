cbf373638465ab15254c978c41299b8bc216d63b
陈卫卫 <2033138908@qq.com>
2023-09-23 21:27:43 +0800

    feat: 收藏列表页面搭建

---
+++ components/tab.vue
@@ -0,0 +1,49 @@
+<template>
+	<u-tabs :list="list1" @click="click" lineWidth="41" lineColor="#FFD100" :activeStyle="{
+		            color: '#232624',
+		            fontWeight: 'bold',
+		            transform: 'scale(1.05)'
+		        }" :inactiveStyle="{
+		            color: '#949494',
+		            transform: 'scale(1)'
+		        }" itemStyle="padding-left: 28rpx; padding-right: 28rpx; height: 88rpx;"></u-tabs>
+</template>
+
+<script>
+	export default {
+		options: {
+			styleIsolation: 'shared',
+		},
+		data() {
+			return {
+				list1: [{
+					name: '闲置',
+				}, {
+					name: '资讯',
+				}, {
+					name: '兼职'
+				}, {
+					name: '寻人寻物'
+				}, {
+					name: '大件清运'
+				}, {
+					name: '房屋转让'
+				}, {
+					name: '闲置交易'
+				}, {
+					name: '同城活动'
+				}, {
+					name: '闲置专区'
+				}]
+			}
+		},
+		methods: {
+			click(item) {
+				console.log('item', item);
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/user/collection/collection.vue
+++ pages/user/collection/collection.vue
@@ -1,21 +1,44 @@
 <template>
 	<view class="collection-page">
 		<view class="tabs">
-			<u-tabs :list="list1" @click="click" lineWidth="41" lineColor="#FFD100" :activeStyle="{
-				            color: '#232624',
-				            fontWeight: 'bold',
-				            transform: 'scale(1.05)'
-				        }" :inactiveStyle="{
-				            color: '#949494',
-				            transform: 'scale(1)'
-				        }" itemStyle="padding-left: 28rpx; padding-right: 28rpx; height: 88rpx;"></u-tabs>
+			<tab></tab>
 		</view>
-
+		<view class="list-container">
+			<view class="user-item">
+				<view class="logo">
+					<image class="image" src="../../../static/chat/avatar.png" mode=""></image>
+				</view>
+				<view class="content">
+					<view class="title">
+						出闲置全新Apple/IPhone15一台,出闲置全新Apple/IPhone15一台。
+					</view>
+					<view class="price">
+						<text class="unit">¥</text>
+						<text>279000</text>
+					</view>
+					<view class="footer">
+						<view class="left">
+							<image class="avatar" src="../../../static/chat/avatar.png" mode=""></image>
+							<view class="nickname">
+								热量计算器
+							</view>
+						</view>
+						<view class="btn">
+							<button class="add">联系TA</button>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import tab from "@/components/tab.vue"
 	export default {
+		components: {
+			tab
+		},
 		options: {
 			styleIsolation: 'shared',
 		},
@@ -42,11 +65,7 @@
 				}]
 			}
 		},
-		methods: {
-			click(item) {
-				console.log('item', item);
-			}
-		}
+
 	}
 </script>
 
@@ -55,9 +74,93 @@
 		background-color: #F3F6F5;
 
 		.tabs {
+			background-color: #ffffff;
 			margin-bottom: 20rpx;
-			::v-deep .u-tabs {
-				background-color: #ffffff;
+
+		}
+
+		.list-container {
+			background-color: #ffffff;
+			padding: 32rpx 28rpx;
+
+			.user-item {
+				display: flex;
+
+				.logo {
+					margin-right: 21rpx;
+
+					.image {
+						width: 188rpx;
+						height: 188rpx;
+						border-radius: 10rpx;
+					}
+				}
+
+				.content {
+					flex: 1;
+					font-family: PingFangSC-Regular, PingFang SC;
+
+					.title {
+						font-size: 28rpx;
+						font-weight: 400;
+						color: #232624;
+						width: 456rpx;
+
+						white-space: nowrap;
+						text-overflow: ellipsis;
+						overflow: hidden;
+					}
+
+					.price {
+						margin: 20rpx;
+						font-size: 30rpx;
+						font-weight: 500;
+						color: #D30303;
+
+						.unit {
+							font-size: 20rpx;
+						}
+					}
+
+					.footer {
+						display: flex;
+						justify-content: space-between;
+						align-items: center;
+
+						.left {
+							display: flex;
+							align-items: center;
+
+							.avatar {
+								width: 36rpx;
+								height: 36rpx;
+								border-radius: 18rpx;
+								margin-right: 9rpx;
+							}
+
+							.nickname {
+								font-size: 22rpx;
+								font-weight: 500;
+								color: #4D504F;
+							}
+						}
+
+						.btn {
+							::v-deep .add {
+								width: 126rpx;
+								height: 58rpx;
+								background: #FFD100;
+								border-radius: 29rpx;
+
+								padding: 0;
+								font-size: 25rpx;
+								font-family: PingFangSC-Regular, PingFang SC;
+								font-weight: 400;
+								color: #4D504F;
+							}
+						}
+					}
+				}
 			}
 		}
 	}
