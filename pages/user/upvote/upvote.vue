66374f0e36277f9bcc2a6562ee46003353008db8
陈卫卫 <2033138908@qq.com>
2023-09-23 21:39:04 +0800

    feat: 点赞列表页面搭建

--- pages/user/upvote/upvote.vue
+++ pages/user/upvote/upvote.vue
@@ -1,22 +1,76 @@
 <template>
-	<view>
-		点赞列表
+	<view class="upvote-page">
+		<view class="tabs">
+			<tab></tab>
+		</view>
+		<view class="list-container">
+			<userItem></userItem>
+			<userItem></userItem>
+			<userItem></userItem>
+			<userItem></userItem>
+			<userItem></userItem>
+			<userItem></userItem>
+			<userItem></userItem>
+			<userItem></userItem>
+			<userItem></userItem>
+			<userItem></userItem>
+		</view>
 	</view>
 </template>
 
 <script>
+	import tab from "@/components/tab.vue"
+	import userItem from "@/components/user_item.vue"
 	export default {
+		components: {
+			tab,
+			userItem
+		},
+		options: {
+			styleIsolation: 'shared',
+		},
 		data() {
 			return {
-				
+				list1: [{
+					name: '闲置',
+				}, {
+					name: '资讯',
+				}, {
+					name: '兼职'
+				}, {
+					name: '寻人寻物'
+				}, {
+					name: '大件清运'
+				}, {
+					name: '房屋转让'
+				}, {
+					name: '闲置交易'
+				}, {
+					name: '同城活动'
+				}, {
+					name: '闲置专区'
+				}]
 			}
 		},
-		methods: {
-			
-		}
+
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.upvote-page {
+		background-color: #F3F6F5;
 
+		.tabs {
+			background-color: #ffffff;
+			margin-bottom: 20rpx;
+
+		}
+
+		.list-container {
+			background-color: #ffffff;
+			padding: 32rpx 28rpx;
+
+
+		}
+	}
 </style>
