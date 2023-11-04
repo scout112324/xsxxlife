5823efdbcbde852455d059b7733db82dda880299
陈卫卫 <2033138908@qq.com>
2023-11-04 21:18:39 +0800

    feat: 寻人寻物地址更改

--- wechatMiniProgram/pages/index/service/modules/lookForPeoplePublish/lookForPeoplePublish.vue
+++ wechatMiniProgram/pages/index/service/modules/lookForPeoplePublish/lookForPeoplePublish.vue
@@ -13,7 +13,9 @@
 				</view>
 				<view class="address">
 					<image class="dingwei" src="../../../../../static/home/dingwei.png" mode=""></image>
-					<u--input class="address-name" placeholder="请输入地址" border="none" v-model="place"></u--input>
+					<uni-data-picker ellipsis popup-title="请选择所在地区" :localdata="areaTree" v-model="place"
+						@change="onchange" :clear-icon="false">
+					</uni-data-picker>
 					<image class="tiaozhuan" src="../../../../../static/unused/tiaozhuan.png" mode=""></image>
 				</view>
 			</view>
@@ -38,6 +40,9 @@
 		addFind,
 		updateFind
 	} from "@/api/index/index.js"
+	import {
+		areaData
+	} from "@/utils/area.js"
 	export default {
 		components: {
 			caremaItem
@@ -46,7 +51,15 @@
 			return {
 				cameraNumber: 9,
 				content: "",
-				place: "",
+				place: [{
+						text: `${uni.getStorageSync('province')}`,
+						value: `${uni.getStorageSync('province')}`
+					},
+					{
+						text: `${uni.getStorageSync('district')}`,
+						value: `${uni.getStorageSync('district')}`
+					}
+				],
 				picture: "",
 				userInfo: {
 					phone: '',
@@ -71,7 +84,8 @@
 				itemData: {},
 				keydata: '1',
 				mediaList: [],
-				itemId: ""
+				itemId: "",
+				areaTree: [],
 			}
 		},
 		onReady() {
@@ -79,25 +93,38 @@
 			this.$refs.uForm.setRules(this.rules)
 		},
 		onLoad(options) {
+			this.areaTree = areaData
 			if (JSON.stringify(options) != "{}") {
 				this.itemData = JSON.parse(decodeURIComponent(options.itemData))
 			}
 		},
 		mounted() {
-			if (this.itemData) {
+			if (JSON.stringify(this.itemData) != "{}") {
 				this.keydata++;
 				let obj = {
 					phone: this.itemData.phone,
 				}
 				this.userInfo = obj
 				this.content = this.itemData.content
-				this.place = this.itemData.place
+				this.place = this.itemData.place.split(',').map(item => {
+					return {
+						text: item,
+						value: item
+					}
+				})
 				this.mediaList = this.itemData.picture ? this.itemData.picture.split(',') : []
 				this.picture = this.mediaList
 				this.itemId = this.itemData.id
 			}
 		},
 		methods: {
+			onchange(e) {
+				this.place = []
+				e.detail.value.forEach(item => {
+					return this.place.push(item.text)
+				})
+				this.place = this.place.toString()
+			},
 			keyboardheightchange(event) {
 				this.bottomHeight = event.detail.height
 			},
@@ -113,7 +140,9 @@
 						if (this.itemId) {
 							let params = {
 								id: this.itemId,
-								place: this.place,
+								place: typeof(this.place) == "string" ? this.place : this.place.map(item => {
+									return item.text
+								}).toString(),
 								content: this.content,
 								picture: this.picture ? this.picture.toString() : "",
 								phone: this.userInfo.phone,
@@ -218,7 +247,7 @@
 				}
 
 				.address {
-					width: 209rpx;
+					width: 80%;
 					height: 46rpx;
 					background: #F1F1F1;
 					border-radius: 20rpx;
@@ -235,12 +264,13 @@
 						height: 21rpx;
 					}
 
-					.address-name {
-						font-size: 22rpx;
-						font-family: PingFangSC-Regular, PingFang SC;
-						font-weight: 400;
-						color: #232624;
-						line-height: 30rpx;
+					::v-deep .input-value-border {
+						width: 100%;
+						border: none;
+					}
+
+					::v-deep .arrow-area {
+						display: none !important;
 					}
 
 					.tiaozhuan {
