b7283eea3b819c2b63d84fa4579ee80a10922cf2
陈卫卫 <2033138908@qq.com>
2024-05-26 10:50:37 +0800

    新需求调整

---
+++ wechatMiniProgram/pages/index/service/notice/notice.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="detail-page">
+		<view class="container">
+			<view class="title">
+				{{noticeData.title}}
+			</view>
+			<view class="content" v-html="noticeData.content"></view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+		articleTo
+	} from '@/api/index/index.js'
+	export default {
+		data() {
+			return {
+				noticeData: {}
+			}
+		},
+		onShow() {
+			this.getArticleTo()
+		},
+		methods: {
+			// 公告跳转页面内容（小程序）
+			getArticleTo() {
+				articleTo().then(res => {
+					if (res.code === 200) {
+						this.noticeData = res.data
+					}
+				})
+			},
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.detail-page {
+	background-color: #F3F6F5;
+	font-family: PingFangSC-Medium, PingFang SC;
+	.container {
+		background-color: #ffffff;
+		padding: 32rpx 30rpx 20rpx 30rpx;
+		
+		.title {
+			font-size: 38rpx;
+			font-weight: bold;
+			color: #000000;
+			margin-bottom: 32rpx;
+		}
+		
+		.content {
+			font-size: 30rpx;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #232624;
+			line-height: 44rpx;
+			margin: 25rpx 0;
+			min-height: 650rpx;
+		}
+	}
+}
+</style>
